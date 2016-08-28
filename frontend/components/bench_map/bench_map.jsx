9edf180f1ee3396f1b062a72aff0f156c5bf83b5
Davin Kim <davinkim@Davins-MacBook-Air.local>
2016-08-28 00:30:36 -0700

    Fifth commit: Got the map rendered on the page.

--- frontend/components/bench_map/bench_map.jsx
+++ frontend/components/bench_map/bench_map.jsx
@@ -5,11 +5,24 @@ class BenchMap extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    const mapDOMNode = this.refs.map;
+
+    const mapOptions = {
+      center: {lat: 37.7758, lng: -122.435},
+      zoom: 13
+    };
+
+    this.map = new google.maps.Map(mapDOMNode, mapOptions);
+  }
+
   render() {
     return(
-      <div id="map-container" ref="map">This is the map!</div>
+      <div id="map-container" ref="map"></div>
     );
   }
 }
 
 export default BenchMap;
+
+// AIzaSyCQBxiR0WdkiWU6Fj3RNQNrOMqkmg7hD_U
